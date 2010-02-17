666e548b66e9b774d8fa4752079350536408f157
Bryan Fink <bryan@basho.com>
2010-02-17 14:08:29 -0500

    switch from copying webmachine source to using rebar deps also make start.sh == start-dev.sh, and remove start-dev.sh

--- priv/templates/start.sh
+++ priv/templates/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/apps/*/ebin -boot start_sasl -s {{appid}}
+exec erl -pa $PWD/ebin $PWD/deps/webmachine/ebin $PWD/deps/webmachine/deps/mochiweb/ebin -boot start_sasl -s reloader -s {{appid}}
