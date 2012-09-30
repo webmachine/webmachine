7feaa6a01e2b21e674bad160e9922c64a8b27d49
Reid Draper <reiddraper@gmail.com>
2012-09-30 13:13:25 -0400

    Equiv tests starting to pass

--- src/webmachine_multipart.erl
+++ src/webmachine_multipart.erl
@@ -19,6 +19,14 @@
 -module(webmachine_multipart).
 -author('Justin Sheehy <justin@basho.com>').
 -author('Andy Gross <andy@basho.com>').
+
+
+-define(HEADERS_BOUNDARY, "\\r\\n\\r\\n").
+
+-ifdef(TEST).
+-compile(export_all).
+-endif.
+
 -export([get_all_parts/2,
          get_all_parts_from_chunked/2,
          stream_parts/2,
@@ -64,7 +72,10 @@ get_all_parts(Body, Boundary) when is_binary(Body), is_list(Boundary) ->
 %%  {part_headers, fpartname(), {[fparam()], [fheader()]}, function()} |
 %%  {part_chunk, fpartname(), fcontent(), function()}.
 stream_parts_chunked(StreamStruct, Boundary) ->
+    %%stream_until_headers(StreamStruct, "\r\n--" ++ Boundary, <<>>).
     stream_until_headers(StreamStruct, "--" ++ Boundary, <<>>).
+
+%% Testing funs ---------------------------------------------------------------
 
 get_all_parts_from_chunked(StreamStruct, Boundary) ->
     handle_stream_parts_chunked(stream_parts_chunked(StreamStruct, Boundary),
@@ -72,93 +83,156 @@ get_all_parts_from_chunked(StreamStruct, Boundary) ->
 
 %% @private
 handle_stream_parts_chunked(done_parts, ResultBuildup) ->
+    %% we're not going to receive any more parts,
+    %% return the result.
     lists:reverse(ResultBuildup);
+handle_stream_parts_chunked({part_headers, PartName, ParamsAndHeaders, done_parts},
+                            ResultBuildup) ->
+    Part = make_fpart(PartName, ParamsAndHeaders, <<>>),
+    lists:reverse([Part | ResultBuildup]);
 handle_stream_parts_chunked({part_headers, PartName, ParamsAndHeaders, NextFun},
                             ResultBuildup) ->
     {NextHeaders, Content} = stream_rest_of_content(NextFun(), []),
-    Part = make_fpart(PartName, ParamsAndHeaders, Content),
-    handle_stream_parts_chunked(NextHeaders, [Part | ResultBuildup]).
+    case NextHeaders of
+        done_parts ->
+            Part = make_fpart(PartName, ParamsAndHeaders, Content),
+            lists:reverse([Part | ResultBuildup]);
+        Else ->
+            Part = make_fpart(PartName, ParamsAndHeaders, Content),
+            handle_stream_parts_chunked(Else, [Part | ResultBuildup])
+    end.
 
+stream_rest_of_content(done_parts, ContentBuffer) ->
+    {done_parts, iolist_to_binary(lists:reverse(ContentBuffer))};
 stream_rest_of_content({part_headers, _PartName, _ParamsAndHeaders, _NextFun}=H,
                        ContentBuffer) ->
-    {H, lists:reverse(ContentBuffer)};
+    {H, iolist_to_binary(lists:reverse(ContentBuffer))};
+stream_rest_of_content({part_chunk, _PartName, Chunk, done_parts}, ContentBuffer) ->
+    {done_parts, iolist_to_binary(lists:reverse([Chunk | ContentBuffer]))};
 stream_rest_of_content({part_chunk, _PartName, Chunk, NextFun}, ContentBuffer) ->
     stream_rest_of_content(NextFun(), [Chunk | ContentBuffer]).
 
 make_fpart(PartName, ParamsAndHeaders, Content) ->
-    {PartName, {ParamsAndHeaders, Content}}.
+    {PartName, ParamsAndHeaders, Content}.
 
 %% Header Streaming -----------------------------------------------------------
 
 %% @private
 stream_until_headers({Hunk, Next}, Boundary, Buffer) ->
     FullBuffer = <<Buffer/binary, Hunk/binary>>,
-    handle_split_headers(re:split(FullBuffer, "\\r\\n\\r\\n", [{parts, 2}]),
+    handle_split_headers(re:split(FullBuffer, ?HEADERS_BOUNDARY, [{parts, 2}]),
                          Boundary, Next).
 
 %% @private
-handle_split_headers([_NoMatch], _Boundary, really_done) ->
+handle_split_headers([<<"--">>], _Boundary, done) ->
+    done_parts;
+handle_split_headers([<<"--\r\n">>], _Boundary, done) ->
+    done_parts;
+handle_split_headers([_NoMatch], _Boundary, done) ->
     %% TODO: think this should be an error
     %% because there is no more data, and we haven't
     %% been able to split the headers
-    ok;
+    error;
 handle_split_headers([NoMatch], Boundary, Next) ->
     %% Next() is safe to call because we pattern
     %% matched on it being `really_done' earlier
     stream_until_headers(Next(), Boundary, NoMatch);
-handle_split_headers([Headers | BodyBegin], Boundary, Next) ->
+handle_split_headers([Headers, BodyBegin], Boundary, Next) ->
     make_headers_response(Headers, BodyBegin, Boundary, Next).
 
 %% @private
-make_headers_response(Headers, BodyBegin, Boundary, really_done) ->
+make_headers_response(Headers, BodyBegin, Boundary, done) ->
     Next = constantly_really_done(),
     full_header_received_response(Headers, BodyBegin, Boundary, Next);
 make_headers_response(Headers, BodyBegin, Boundary, Next) ->
     full_header_received_response(Headers, BodyBegin, Boundary, Next).
 
 full_header_received_response(Headers, BodyBegin, Boundary, Next) ->
+    RestHeaders = case re:split(Headers, Boundary, [{parts, 2}]) of
+        [Single] -> Single;
+        [<<>>, Rest] -> Rest
+    end,
     HeadList = [list_to_binary(X) ||
-                   X <- string:tokens(binary_to_list(Headers), "\r\n")],
-    {Name, Params, Headers} = make_headers(HeadList),
-    {part_headers, Name, {Params, Headers}, fun () ->
+                   X <- string:tokens(binary_to_list(RestHeaders), "\r\n")],
+    {Name, Params, ActualHeaders} = make_headers(HeadList),
+    {part_headers, Name, {Params, ActualHeaders}, fun () ->
                 stream_rest_of_value({BodyBegin, Next},
                                      Boundary,
                                      Name) end}.
 
 constantly_really_done() ->
-    fun () -> {<<>>, really_done} end.
+    fun () -> {<<>>, done} end.
 
 %% Value Streaming ------------------------------------------------------------
 
 %% @private
 stream_rest_of_value({Hunk, Next}, Boundary, PartName) ->
-    handle_split_boundary(re:split(Hunk, Boundary, [{parts, 2}]),
-                          Boundary, PartName, Next).
+    Split = re:split(Hunk, Boundary, [{parts, 2}]),
+    handle_split_boundary(Split, Boundary, PartName, Next).
 
 %% @private
-handle_split_boundary([_NoMatch], _Boundary, _PartName, really_done) ->
+handle_split_boundary([_NoMatch], _Boundary, _PartName, done) ->
     %% this is an error, because we haven't been able to make a full
     %% body out of what is here and there is no more data.
-    ok;
+    error;
 handle_split_boundary([NoMatch], Boundary, PartName, Next) ->
     %% Next() should be safe to call
-    {part_chunk, PartName, NoMatch, fun () ->
-                stream_rest_of_value(Next(), Boundary, PartName) end};
-handle_split_boundary([RestBinary | BeginHeaders], Boundary, PartName, really_done) ->
+    CRLF = <<"\r\n">>,
+    BinaryBoundary = list_to_binary(Boundary),
+    BinaryBoundaryWithCRLF = <<CRLF/binary, BinaryBoundary/binary>>,
+    case could_possibly_contain_boundary(NoMatch, BinaryBoundaryWithCRLF) of
+        true ->
+            {NewHunk, Next2} = Next(),
+            NewHunk2 = <<NoMatch/binary, NewHunk/binary>>,
+            stream_rest_of_value({NewHunk2, Next2}, Boundary, PartName);
+        false ->
+            {part_chunk, PartName, NoMatch, fun () ->
+                        stream_rest_of_value(Next(), Boundary, PartName) end}
+    end;
+handle_split_boundary([RestBinary, BeginHeaders], Boundary, PartName, done) ->
     Next = constantly_really_done(),
-    make_chunk_response(PartName, RestBinary, Boundary, Next, BeginHeaders);
-handle_split_boundary([RestBinary | BeginHeaders], Boundary, PartName, Next) ->
-    make_chunk_response(PartName, RestBinary, Boundary, Next, BeginHeaders).
+    make_chunk_response(PartName, remove_crlf(RestBinary),
+                        Boundary, Next, BeginHeaders);
+handle_split_boundary([RestBinary, BeginHeaders], Boundary, PartName, Next) ->
+    make_chunk_response(PartName, remove_crlf(RestBinary),
+                        Boundary, Next, BeginHeaders).
 
+%% @private
+%% much easier to assume Boundary is a binary here
+-spec could_possibly_contain_boundary(binary(), binary()) -> boolean().
+could_possibly_contain_boundary(Chunk, Boundary) ->
+    could_be_suffix(Chunk, Boundary).
+
+%% Could the end of A contain the beginning of B?
+could_be_suffix(A, B) ->
+    Pred = fun (Prefix) ->
+            binary:longest_common_suffix([A, Prefix]) > 0 end,
+    lists:any(Pred, prefixes(B)).
+
+prefixes(Bin) ->
+    Max = size(Bin) - 1,
+    [binary:part(Bin, {0, N}) || N <- lists:seq(1, Max)].
+
+make_chunk_response(PartName, RestBinary, _Boundary, _Next, <<"--">>) ->
+    {part_chunk, PartName, RestBinary, done_parts};
+make_chunk_response(PartName, RestBinary, _Boundary, _Next, <<"--\r\n">>) ->
+    {part_chunk, PartName, RestBinary, done_parts};
 make_chunk_response(PartName, RestBinary, Boundary, Next, Buffer) ->
-    {part_chunk, PartName, remove_crlf(RestBinary), fun () ->
+    {part_chunk, PartName, RestBinary, fun () ->
                 stream_until_headers(Next(), Boundary, Buffer) end}.
 
 %% @private
+remove_crlf(<<>>) ->
+    <<>>;
 remove_crlf(Binary) ->
     BodyLen = size(Binary) - 2,
-    <<WithoutCRLF:BodyLen/binary, _/binary>> = Binary,
-    WithoutCRLF.
+    CRLF = <<"\r\n">>,
+    case Binary of
+        <<WithoutCRLF:BodyLen/binary, CRLF/binary>> ->
+            WithoutCRLF;
+        _NoMatch ->
+            Binary
+    end.
 
 % @doc Similar to get_all_parts/2, but for streamed/chunked bodies.
 %   Takes as input the result of wrq:stream_req_body/2, and provides
@@ -202,7 +276,7 @@ get_more_data(Fun) -> Fun().
    
 make_part(PartData) ->
     %% Remove the trailing \r\n
-    [HeadData, BodyWithCRLF] = re:split(PartData, "\\r\\n\\r\\n", [{parts,2}]),
+    [HeadData, BodyWithCRLF] = re:split(PartData, ?HEADERS_BOUNDARY, [{parts,2}]),
     BodyLen = size(BodyWithCRLF) - 2,
     <<Body:BodyLen/binary, _/binary>> = BodyWithCRLF,
 
@@ -256,9 +330,24 @@ send_streamed_body(Body, Max) ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
+%% body for `body_test/0'
+body_1() ->
+    {<<"------------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2\r\nContent-Disposition: form-data; name=\"Filename\"\r\n\r\ntestfile.txt\r\n------------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2\r\nContent-Disposition: form-data; name=\"Filedata\"; filename=\"testfile.txt\"\r\nContent-Type: application/octet-stream\r\n\r\n%%% The contents of this file are a test,\n%%% do not be alarmed.\n\r\n------------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2\r\nContent-Disposition: form-data; name=\"Upload\"\r\n\r\nSubmit Query\r\n------------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2--">>, "----------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2"}.
+
+body_2() ->
+    {<<"-----------------------------89205314411538515011004844897\r\nContent-Disposition: form-data; name=\"Filedata\"; filename=\"akamai.txt\"\r\nContent-Type: text/plain\r\n\r\nCAMBRIDGE, MA - February 18, 2009 - Akamai Technologies, Inc. (NASDAQ: AKAM), the leader in powering rich media, dynamic transactions and enterprise applications online, today announced that its Service & Support organization was awarded top honors for Innovation in Customer Service at the 3rd Annual Stevie Awards for Sales & Customer Service, an international competition recognizing excellence in disciplines that are crucial to business success.\n\n\"We have always set incredibly high standards with respect to the service and support we provide our customers,\" said Sanjay Singh, vice president of Global Service & Support at Akamai. \"Our support team provides highly responsive service around the clock to our global customer base and, as a result, has become an extension of our customers' online businesses. This prestigious award is validation of Akamai's commitment to customer service and technical support.\"\n\nAkamai Service & Support professionals are dedicated to working with customers on a daily basis to fine tune, optimize, and support their Internet initiatives. Akamai's winning submission highlighted the key pillars of its service and support offering, as well as the initiatives established to meet customer requirements for proactive communication, simplification, and faster response times.\n\n\"This year's honorees demonstrate that even in challenging economic times, it's possible for organizations to continue to shine in sales and customer service, the two most important functions in business: acquiring and keeping customers,\" said Michael Gallagher, president of the Stevie Awards.\n\nThe awards are presented by the Stevie Awards, which organizes several of the world's leading business awards shows, including the prestigious American Business Awards. Nicknamed the Stevies for the Greek word \"crowned,\" winners were announced during a gala banquet on Monday, February 9 at Caesars Palace in Las Vegas. Nominated customer service and sales executives from the U.S.A. and several other countries attended. More than 500 entries from companies of all sizes and in virtually every industry were submitted to this year's competition. There are 27 categories for customer service professionals, as well as 41 categories for sales professionals.\n\nDetails about the Stevie Awards for Sales & Customer Service and the list of honorees in all categories are available at www.stevieawards.com/sales. \n\r\n-----------------------------89205314411538515011004844897--\r\n">>,
+    "---------------------------89205314411538515011004844897"}.
+
+firefox_body() ->
+    {<<"-----------------------------823378840143542612896544303\r\nContent-Disposition: form-data; name=\"upload-test\"; filename=\"abcdef.txt\"\r\nContent-Type: text/plain\r\n\r\n01234567890123456789012345678901234567890123456789\r\n-----------------------------823378840143542612896544303--\r\n">>,
+     "---------------------------823378840143542612896544303"}.
+
+chrome_body() ->
+    {<<"------WebKitFormBoundaryIHB9Xyi7ZCNKJusP\r\nContent-Disposition: form-data; name=\"upload-test\"; filename=\"abcdef.txt\"\r\nContent-Type: text/plain\r\n\r\n01234567890123456789012345678901234567890123456789\r\n------WebKitFormBoundaryIHB9Xyi7ZCNKJusP--\r\n">>,
+     "----WebKitFormBoundaryIHB9Xyi7ZCNKJusP"}.
+
 body_test() ->
-    Body = <<"------------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2\r\nContent-Disposition: form-data; name=\"Filename\"\r\n\r\ntestfile.txt\r\n------------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2\r\nContent-Disposition: form-data; name=\"Filedata\"; filename=\"testfile.txt\"\r\nContent-Type: application/octet-stream\r\n\r\n%%% The contents of this file are a test,\n%%% do not be alarmed.\n\r\n------------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2\r\nContent-Disposition: form-data; name=\"Upload\"\r\n\r\nSubmit Query\r\n------------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2--">>,
-    Boundary = "----------ae0gL6gL6Ij5KM7Ef1KM7ei4ae0cH2",
+    {Body, Boundary} = body_1(),
     ?assertEqual(
        [{"Filename",
          {[{<<"name">>,<<"Filename">>}],[]},
@@ -273,8 +362,7 @@ body_test() ->
        get_all_parts(Body, Boundary)).
     
 body2_test() ->
-    Body = <<"-----------------------------89205314411538515011004844897\r\nContent-Disposition: form-data; name=\"Filedata\"; filename=\"akamai.txt\"\r\nContent-Type: text/plain\r\n\r\nCAMBRIDGE, MA - February 18, 2009 - Akamai Technologies, Inc. (NASDAQ: AKAM), the leader in powering rich media, dynamic transactions and enterprise applications online, today announced that its Service & Support organization was awarded top honors for Innovation in Customer Service at the 3rd Annual Stevie Awards for Sales & Customer Service, an international competition recognizing excellence in disciplines that are crucial to business success.\n\n\"We have always set incredibly high standards with respect to the service and support we provide our customers,\" said Sanjay Singh, vice president of Global Service & Support at Akamai. \"Our support team provides highly responsive service around the clock to our global customer base and, as a result, has become an extension of our customers' online businesses. This prestigious award is validation of Akamai's commitment to customer service and technical support.\"\n\nAkamai Service & Support professionals are dedicated to working with customers on a daily basis to fine tune, optimize, and support their Internet initiatives. Akamai's winning submission highlighted the key pillars of its service and support offering, as well as the initiatives established to meet customer requirements for proactive communication, simplification, and faster response times.\n\n\"This year's honorees demonstrate that even in challenging economic times, it's possible for organizations to continue to shine in sales and customer service, the two most important functions in business: acquiring and keeping customers,\" said Michael Gallagher, president of the Stevie Awards.\n\nThe awards are presented by the Stevie Awards, which organizes several of the world's leading business awards shows, including the prestigious American Business Awards. Nicknamed the Stevies for the Greek word \"crowned,\" winners were announced during a gala banquet on Monday, February 9 at Caesars Palace in Las Vegas. Nominated customer service and sales executives from the U.S.A. and several other countries attended. More than 500 entries from companies of all sizes and in virtually every industry were submitted to this year's competition. There are 27 categories for customer service professionals, as well as 41 categories for sales professionals.\n\nDetails about the Stevie Awards for Sales & Customer Service and the list of honorees in all categories are available at www.stevieawards.com/sales. \n\r\n-----------------------------89205314411538515011004844897--\r\n">>,
-    Boundary = "---------------------------89205314411538515011004844897",
+    {Body, Boundary} = body_2(),
     ?assertEqual(
        [{"Filedata",
          {[{<<"name">>,<<"Filedata">>},
@@ -285,8 +373,7 @@ body2_test() ->
        get_all_parts(Body,Boundary)).
 
 firefox_test() ->
-    Body = <<"-----------------------------823378840143542612896544303\r\nContent-Disposition: form-data; name=\"upload-test\"; filename=\"abcdef.txt\"\r\nContent-Type: text/plain\r\n\r\n01234567890123456789012345678901234567890123456789\r\n-----------------------------823378840143542612896544303--\r\n">>,
-    Boundary = "---------------------------823378840143542612896544303",
+    {Body, Boundary} = firefox_body(),
     ?assertEqual(
        [{"upload-test",
          {[{<<"name">>,<<"upload-test">>},
@@ -296,8 +383,7 @@ firefox_test() ->
        get_all_parts(Body,Boundary)).
 
 chrome_test() ->
-    Body = <<"------WebKitFormBoundaryIHB9Xyi7ZCNKJusP\r\nContent-Disposition: form-data; name=\"upload-test\"; filename=\"abcdef.txt\"\r\nContent-Type: text/plain\r\n\r\n01234567890123456789012345678901234567890123456789\r\n------WebKitFormBoundaryIHB9Xyi7ZCNKJusP--\r\n">>,
-    Boundary = "----WebKitFormBoundaryIHB9Xyi7ZCNKJusP",
+    {Body, Boundary} = chrome_body(),
     ?assertEqual(
        [{"upload-test",
          {[{<<"name">>,<<"upload-test">>},
@@ -305,5 +391,32 @@ chrome_test() ->
           [{<<"Content-Type">>,<<"text/plain">>}]},
          <<"01234567890123456789012345678901234567890123456789">>}],
        get_all_parts(Body,Boundary)).
+
+%% Test equivalence of chunked and old tests
+
+body_equiv_test() ->
+    {Body, Boundary} = body_1(),
+    StreamAdapter = send_streamed_body(Body, 1),
+    ?assertEqual(get_all_parts(Body, Boundary),
+                 get_all_parts_from_chunked(StreamAdapter, Boundary)).
+
+body_2_equiv_test() ->
+    {Body, Boundary} = body_2(),
+    StreamAdapter = send_streamed_body(Body, 1),
+    ?assertEqual(get_all_parts(Body, Boundary),
+                 get_all_parts_from_chunked(StreamAdapter, Boundary)).
+
+firefox_equiv_test() ->
+    {Body, Boundary} = firefox_body(),
+    StreamAdapter = send_streamed_body(Body, 1),
+    ?assertEqual(get_all_parts(Body, Boundary),
+                 get_all_parts_from_chunked(StreamAdapter, Boundary)).
+
+chrome_equiv_test() ->
+    {Body, Boundary} = chrome_body(),
+    StreamAdapter = send_streamed_body(Body, 1),
+    ?assertEqual(get_all_parts(Body, Boundary),
+                 get_all_parts_from_chunked(StreamAdapter, Boundary)).
+
 
 -endif.
