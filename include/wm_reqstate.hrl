6bbae8a4f9773fa2ffdff0a3f3ca33b0fa1b91d7
UENISHI Kota <kuenishi@gmail.com>
2013-03-05 16:46:29 +0900

    adding type/spec info to wm_reqdata

--- include/wm_reqstate.hrl
+++ include/wm_reqstate.hrl
@@ -1,11 +1,11 @@
--record(wm_reqstate, {socket=undefined,
-                   metadata=orddict:new(),
-                   range=undefined,
-                   peer=undefined,
-                   sock=undefined,
-                   reqdata=undefined,
-                   bodyfetch=undefined,
-                   reqbody=undefined,
-                   log_data=undefined
-                  }).
+-record(wm_reqstate, {socket=undefined :: undefined | inet:socket(),
+                      metadata=orddict:new() :: orddict:orddict(),
+                      range=undefined     :: undefined | string(),
+                      peer=undefined      :: undefined | inet:ip_address(),
+                      sock=undefined,
+                      reqdata=undefined,  %% reqdata
+                      bodyfetch=undefined,
+                      reqbody=undefined   :: undefined | binary(),
+                      log_data=undefined
+                     }).
 
