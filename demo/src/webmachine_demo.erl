5d9fa95d7f8d15cc398f3a0a8c00b26b2b5cdfd1
Nick Marino <nmarino@basho.com>
2014-11-20 13:49:26 -0500

    Fix webmachine_demo start: use ensure_all_started
    Previously we were getting a crash on start due to ssl not being
    started. Using ensure_all_started fixes this, and also allows us to
    remove some of the boilerplate code for manually starting other
    application dependencies.

--- demo/src/webmachine_demo.erl
+++ demo/src/webmachine_demo.erl
@@ -3,35 +3,22 @@
 -author('Justin Sheehy <justin@@basho.com>').
 -export([start/0, start_link/0, stop/0]).
 
-ensure_started(App) ->
-    case application:start(App) of
-        ok ->
-            ok;
-        {error, {already_started, App}} ->
-            ok
-    end.
-
 %% @spec start_link() -> {ok,Pid::pid()}
 %% @doc Starts the app for inclusion in a supervisor tree
 start_link() ->
-    ensure_started(crypto),
-    ensure_started(mochiweb),
     LogHandlers = [{webmachine_access_log_handler, ["priv/log"]},
                    {webmachine_error_log_handler, ["priv/log"]}],
     application:set_env(webmachine, log_handlers, LogHandlers),
-    ensure_started(webmachine),
+    webmachine_util:ensure_all_started(webmachine),
     webmachine_demo_sup:start_link().
 
 %% @spec start() -> ok
 %% @doc Start the webmachine_demo server.
 start() ->
-    ensure_started(inets),
-    ensure_started(crypto),
-    ensure_started(mochiweb),
     LogHandlers = [{webmachine_access_log_handler, ["priv/log"]},
                    {webmachine_error_log_handler, ["priv/log"]}],
     application:set_env(webmachine, log_handlers, LogHandlers),
-    ensure_started(webmachine),
+    webmachine_util:ensure_all_started(webmachine),
     application:start(webmachine_demo).
 
 %% @spec stop() -> ok
