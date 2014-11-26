1e5dc51fe2466784efa93c6a9dbbe85ab5a641d9
Jordan West <jrwest@users.noreply.github.com>
2014-11-25 19:52:48 -0800

    Update webmachine_request.erl

--- src/webmachine_request.erl
+++ src/webmachine_request.erl
@@ -101,7 +101,7 @@
 -include("wm_reqdata.hrl").
 
 -define(WMVSN, "1.10.6").
--define(QUIP, "no drinks").
+-define(QUIP, "we're aiming for the pool, right?").
 -define(IDLE_TIMEOUT, infinity).
 
 new(#wm_reqstate{}=ReqState) ->
