7677c240f4a7ed020f4bab48278224966bb42311
Sean Cribbs <sean@basho.com>
2015-02-11 12:38:23 -0600

    Bump to 1.10.8

--- src/webmachine_request.erl
+++ src/webmachine_request.erl
@@ -100,8 +100,8 @@
 -include("wm_reqstate.hrl").
 -include("wm_reqdata.hrl").
 
--define(WMVSN, "1.10.7").
--define(QUIP, "we're aiming for the pool, right?").
+-define(WMVSN, "1.10.8").
+-define(QUIP, "that head fake, tho").
 -define(IDLE_TIMEOUT, infinity).
 
 new(#wm_reqstate{}=ReqState) ->
