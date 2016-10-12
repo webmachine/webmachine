f1c9a480689bfe60b31aff3c8331f871f7794c82
Joe DeVivo <joe@devivo.com>
2016-10-12 09:52:50 -0700

    Dialyzer fixes for `webmachine_mochiweb`

--- src/webmachine.erl
+++ src/webmachine.erl
@@ -17,12 +17,7 @@
 -module(webmachine).
 -author('Justin Sheehy <justin@basho.com>').
 -author('Andy Gross <andy@basho.com>').
--export([start/0, stop/0]).
--export([new_request/2]).
-
--include("webmachine_logger.hrl").
--include("wm_reqstate.hrl").
--include("wm_reqdata.hrl").
+-export([start/0, stop/0, new_request/2]).
 
 %% @spec start() -> ok
 %% @doc Start the webmachine server.
@@ -46,66 +41,8 @@ ensure_started(App) ->
 stop() ->
     application:stop(webmachine).
 
-new_request(mochiweb, Request) ->
-    Method = Request:get(method),
-    Scheme = Request:get(scheme),
-    Version = Request:get(version),
-    {Headers, RawPath} = case application:get_env(webmachine, rewrite_module) of
-        {ok, RewriteMod} ->
-            do_rewrite(RewriteMod,
-                       Method,
-                       Scheme,
-                       Version,
-                       Request:get(headers),
-                       Request:get(raw_path));
-        undefined ->
-            {Request:get(headers), Request:get(raw_path)}
-    end,
-    Socket = Request:get(socket),
-
-    InitialReqData = wrq:create(Method,Scheme,Version,RawPath,Headers),
-    InitialLogData = #wm_log_data{start_time=os:timestamp(),
-                                  method=Method,
-                                  headers=Headers,
-                                  path=RawPath,
-                                  version=Version,
-                                  response_code=404,
-                                  response_length=0},
-
-    InitState = #wm_reqstate{socket=Socket,
-                             log_data=InitialLogData,
-                             reqdata=InitialReqData},
-    InitReq = {webmachine_request,InitState},
-
-    case InitReq:get_peer() of
-      {ErrorGetPeer = {error,_}, ErrorGetPeerReqState} ->
-        % failed to get peer
-        { ErrorGetPeer, webmachine_request:new (ErrorGetPeerReqState) };
-      {Peer, _ReqState} ->
-        case InitReq:get_sock() of
-          {ErrorGetSock = {error,_}, ErrorGetSockReqState} ->
-            LogDataWithPeer = InitialLogData#wm_log_data {peer=Peer},
-            ReqStateWithSockErr =
-              ErrorGetSockReqState#wm_reqstate{log_data=LogDataWithPeer},
-            { ErrorGetSock, webmachine_request:new (ReqStateWithSockErr) };
-          {Sock, ReqState} ->
-            ReqData = wrq:set_sock(Sock, wrq:set_peer(Peer, InitialReqData)),
-            LogData =
-              InitialLogData#wm_log_data {peer=Peer, sock=Sock},
-            webmachine_request:new(ReqState#wm_reqstate{log_data=LogData,
-                                                        reqdata=ReqData})
-        end
-    end.
-
-
-do_rewrite(RewriteMod, Method, Scheme, Version, Headers, RawPath) ->
-    case RewriteMod:rewrite(Method, Scheme, Version, Headers, RawPath) of
-        %% only raw path has been rewritten (older style rewriting)
-        NewPath when is_list(NewPath) -> {Headers, NewPath};
-
-        %% headers and raw path rewritten (new style rewriting)
-        {NewHeaders, NewPath} -> {NewHeaders,NewPath}
-    end.
+new_request(mochiweb, MochiReq) ->
+    webmachine_mochiweb:new_webmachine_req(MochiReq).
 
 %%
 %% TEST
